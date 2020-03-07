57ee64cade085096f81ac4ef5a0de24037fb4cff
Jamie Barton <jamie@notrab.dev>
2020-03-07 08:36:33 +0000

    revert using theme for demo

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-theme-transistorfm',
+      resolve: 'gatsby-source-transistorfm',
       options: {
         url: process.env.TRANSISTOR_URL,
       },
     },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
